a0a053c0354c8b599aa8738f727d99870d434817
telhawasim <telhawasim@gmail.com>
2023-10-22 10:28:14 +0500

    initial commit

---
+++ Practiise/Practiise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practiise
+//
+//  Created by Telha Wasim on 22/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
